0caac9648ab251e10a837ca242a25a79f835a6d8
ZakiyaA <zakiyaalfughi@gmail.com>
2022-01-09 17:55:32 +0000

    Insert data into users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,5 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, password) VALUES ('cstuer0@gmail.com', 'GFPdkiT6gcCv');
+INSERT INTO users (email, password) VALUES ('bhuebner1@yahoo.co', 'cfJM4NPpfU');
+INSERT INTO users (email, password) VALUES ('awombwell2@mail.com', 'fYXud3');
+INSERT INTO users (email, password) VALUES ('rdowdle3@ebay.co.uk', 'dnuxceVtc5I');
+INSERT INTO users (email, password) VALUES ('cpaulley4@cocolog-nifty.com', 'XsMzlb');
