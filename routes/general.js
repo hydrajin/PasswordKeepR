366d79f421ffda253b5a1c965257d8fe8c2590f8
Suliman Fayyaz <hydrajin@gmail.com>
2022-01-10 00:55:28 -0500

    create /new skeleton for generate password

--- routes/general.js
+++ routes/general.js
@@ -9,6 +9,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
+  // Routes to the "Main" page
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM accounts;`)
       .then(data => {
@@ -22,9 +23,36 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
-
+  // Routes to the "Generate Password" page
+  router.get("/new", (req, res) => {
+    db.query(`SELECT * FROM accounts;`)
+      .then(data => {
+        console.log(data.rows);
+        // const templateVars = {accounts:data.rows};
+        res.render("generatePassword");
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  // Shows the .JSON of the accounts db files
   router.get("/accounts", (req, res) => { // /api/users/accounts
     db.query(`SELECT * FROM accounts;`)
+      .then(data => {
+        const accounts = data.rows;
+        res.json({ accounts });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  // shows the .JSON of the users db files
+  router.get("/users", (req, res) => { // /api/users/accounts
+    db.query(`SELECT * FROM users;`)
       .then(data => {
         const users = data.rows;
         res.json({ users });
