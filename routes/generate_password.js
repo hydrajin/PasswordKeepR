51e39a11fac4bf1a045c42c4819b0459391eeb81
ZakiyaA <zakiyaalfughi@gmail.com>
2022-01-13 00:55:04 +0000

    add generate password route

--- routes/generate_password.js
+++ routes/generate_password.js
@@ -14,25 +14,24 @@
 });
 
 
-router.post("/generate/:organization_id", (req, res) => { // for distinct organization
-  const organizationId = req.params.organization_id;
-  const username = req.body.username;
-  const url = req.body.url;
-  const category = req.body.category;
-  db.query(`SELECT * FROM accounts WHERE organization_id = $1;`,[organizationId])
-    .then(data => {
-      const password = generatePassword();
-      const templateVars = { accounts: data.rows, password, organizationId, username, url, category };
-      res.render("createAccount", templateVars);
-      console.log(templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-});
-
+// router.post("/generate/:organization_id", (req, res) => { // for distinct organization
+//   const organizationId = req.params.organization_id;
+//   const username = req.body.username;
+//   const url = req.body.url;
+//   const category = req.body.category;
+//   db.query(`SELECT * FROM accounts WHERE organization_id = $1;`,[organizationId])
+//     .then(data => {
+//       const password = generatePassword();
+//       const templateVars = { accounts: data.rows, password, organizationId, username, url, category };
+//       res.render("createAccount", templateVars);
+//       console.log(templateVars);
+//     })
+//     .catch(err => {
+//       res
+//         .status(500)
+//         .json({ error: err.message });
+//     });
+// });
 
 
 
